65f2b5ffcbd79da6df4ba84e6eaaa119daf1a00e
Lucas <lucasleitep15@gmail.com>
2025-02-25 03:23:45 -0300

    resolvido erro script

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 const perfil = document.querySelector('.perfil');
 const share = document.querySelector('.share');
+const picture = document.getElementById('dark');
 
-console.log(perfil);
-console.log(share);
+console.log(picture);
 
 let isActive = false;
+let larguraTela;
 
-function shareActive() {
-    if (isActive) {
-        perfil.style.display = 'flex';
-        share.style.display = 'none';
-        isActive = false;
+const share_button = document.querySelector('.dark');
+
+function verificarTamanhoTela () {
+    larguraTela = window.innerWidth;
+    if (larguraTela >= 1024) {
+        share_button.remove();
     } else {
-        perfil.style.display = 'none';
-        share.style.display = 'flex';
-        isActive = true;
+        picture.appendChild(share_button);
     }
 }
 
+verificarTamanhoTela();
+
+function shareActive() {
+    if (larguraTela <= 1024) {
+        if (isActive) {
+            perfil.style.display = 'flex';
+            share.style.display = 'none';
+            isActive = false;
+        } else {
+            perfil.style.display = 'none';
+            share.style.display = 'flex';
+            isActive = true;
+        }
+    } else {
+        if (isActive) {
+            share.style.display = 'none';
+            isActive = false;
+        } else {
+            share.style.display = 'flex';
+            isActive = true;
+        }
+    }
+}
+
+window.addEventListener("resize", verificarTamanhoTela);
